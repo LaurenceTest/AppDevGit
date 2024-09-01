9dfb00ffb517fefa014e3fd4b8ab794f517868d0
LaurenceTest <reylaurencecapoy@gmail.com>
2024-09-01 17:14:54 +0800

    Add personalized greeting feature

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
-console.log("Hello world!");
-export {};
+import { stdin, stdout } from 'process';
+import readline from 'readline';
+const rl = readline.createInterface({
+    input: stdin,
+    output: stdout
+});
+rl.question("What's your name? ", (response) => {
+    console.log(`Hello, ${response}!`);
+    rl.close();
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!")
+import { stdin, stdout } from 'process'
+import readline from 'readline'
+
+const rl: readline.Interface = readline.createInterface({
+    input: stdin,
+    output: stdout
+})
+rl.question("What's your name? ",(response)=>{
+    console.log(`Hello, ${response}!`)
+    rl.close()
+})
